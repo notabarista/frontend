4fb397d62ff5c9609be281c7f8f238ab7060a746
unknown <oseeronlan@reimnet.com>
2021-06-18 21:24:20 +0100

    [chore]:initialise store in item module

--- src/app/content/item/item.module.ts
+++ src/app/content/item/item.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
 
 import { ItemOverview } from './item-overview/item-overview.component';
 
-const itemRoutes: Routes = [
-  { path: '', component: ItemOverview }
-];
+const itemRoutes: Routes = [{ path: '', component: ItemOverview }];
 
 @NgModule({
   imports: [
-    RouterModule.forChild(itemRoutes)
+    RouterModule.forChild(itemRoutes),
+    StoreModule.forFeature('item', {}),
   ],
-  declarations: [
-    ItemOverview,]
+  declarations: [ItemOverview],
 })
-export class ItemModule { }
+export class ItemModule {}
